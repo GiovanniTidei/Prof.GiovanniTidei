b9db51ebc34f789707486b63253893be2fee011d
GiovanniTidei <60188364+GiovanniTidei@users.noreply.github.com>
2021-09-15 21:24:36 +0200

    Update footer.js

--- footer.js
+++ footer.js
@@ -33,5 +33,5 @@ function expand() {
 //Velocità audio
 var vid = document.getElementsByTagName("audio");
 for (var i = 0; i < vid.length; i++) {
-    vid[i].playbackRate = 1.2;
+    vid[i].playbackRate = 1.1;
 }
